09a2cdfbe138774d696d90d6811a3b2d40c3f0b3
Your Name <you@example.com>
2017-06-01 11:44:52 +0200

    dodatek

---
+++ trojkacik.js
@@ -0,0 +1,12 @@
+var a = 10 
+var h =	20
+triangleArea = a*h/2
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+// druga wersja
+
+var u = prompt ("wpisz podstawę trojkata")
+var g = prompt ("wpis wysokosc trojkata")
+triangleArea1 = u*g/2
+console.log('Twoje pole trojkata to:' + triangleArea1 + ' Taniej wyjdzie monogamia.');
+
