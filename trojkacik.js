2c536210d1871860cb038a9f7f1c96f934834b06
Your Name <you@example.com>
2017-06-01 12:00:06 +0200

    costam

--- trojkacik.js
+++ trojkacik.js
@@ -1,12 +1,12 @@
-var a = 10 
-var h =	20
-triangleArea = a*h/2
+﻿var a = 10;
+var h =	20;
+triangleArea = a*h/2;
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 
 // druga wersja
 
-var u = prompt ("wpisz podstawę trojkata")
-var g = prompt ("wpis wysokosc trojkata")
-triangleArea1 = u*g/2
-console.log('Twoje pole trojkata to:' + triangleArea1 + ' Taniej wyjdzie monogamia.');
+var u = prompt('wpisz podstawę trojkata');
+var g = prompt('wpisz wysokosc trojkata');
+triangleArea1 = u*g/2;
+console.log('Twoje pole trojkata to: ' + triangleArea1 + '.' ' Taniej wyjdzie monogamia.');
 
